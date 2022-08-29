d422b1f4f05ae36574f51591d0fd0bf4526540bd
Punit Jindal <punit0901.be20@chitkara.edu.in>
2022-08-29 11:18:36 +0530

    committed JavaScript React

--- JavaScript React/react-task-tracker/src/App.js
+++ JavaScript React/react-task-tracker/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from'./component/Header';
+const name = "Punit";
+const x = true;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h1>{name}</h1>
+      <h2>{12+8}</h2>
+      <h2>{x ? "Yes" : "No"}</h2>
     </div>
   );
 }
---
+++ JavaScript React/react-task-tracker/src/component/Header.js
@@ -0,0 +1,8 @@
+const header = () => {
+    return(
+        <header>
+            <h1>Hello Header</h1>
+        </header>
+    );
+};
+export default header;
