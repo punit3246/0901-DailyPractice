f3f9677dbc2d6addcc122e193ba6b6109610f8ef
Punit Jindal <punit0901.be20@chitkara.edu.in>
2022-09-01 10:37:52 +0530

    committed JavaScript React

--- JavaScript React/react-task-tracker/src/App.js
+++ JavaScript React/react-task-tracker/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import Header from'./component/Header';
+import "./App.css";
+import Header from "./component/Header";
 const name = "Punit";
 const x = true;
 function App() {
   return (
     <div className="App">
-      <Header />
+
+      <Header/>
+      {/* <Header title="Punit"/> */}
       <h1>{name}</h1>
       <h2>{12+8}</h2>
       <h2>{x ? "Yes" : "No"}</h2>
--- JavaScript React/react-task-tracker/src/component/Header.js
+++ JavaScript React/react-task-tracker/src/component/Header.js
@@ -1,8 +1,19 @@
-const header = () => {
+import PropTypes from "prop-types";
+const header = (props) => {
     return(
         <header>
-            <h1>Hello Header</h1>
+            <h1>Hello, {props.title}</h1>
         </header>
     );
 };
+header.defaultProps = {
+    title:"Task Tracker",
+};
+
+header.propTypes = {
+    title:PropTypes.string.isRequired,
+};
 export default header;
+
+//const header = ({ title }) =>
+//<h1>hello, {title}</h1>   
