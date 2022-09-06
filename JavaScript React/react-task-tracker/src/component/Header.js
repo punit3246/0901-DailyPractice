850317d5c755178cdb837efd9fc22993818e59a1
Punit Jindal <punit0901.be20@chitkara.edu.in>
2022-09-06 14:36:47 +0530

    committed JavaScript React

--- JavaScript React/react-task-tracker/src/component/Header.js
+++ JavaScript React/react-task-tracker/src/component/Header.js
@@ -1,11 +1,22 @@
 import PropTypes from "prop-types";
+import "./Header.css";
 const header = (props) => {
     return(
         <header>
-            <h1>Hello, {props.title}</h1>
+            <h1 style={headingStyle}>Hello, {props.title}</h1>
+            <h2 className="h2Head">H2 Heading</h2>
+            <h3 id="h3Head">H3 Heading</h3>
+            <button className="btn">Add</button>
         </header>
     );
 };
+
+const headingStyle ={
+    color: "darkcyan",
+    backgroundColor: "lightblue",
+    textAlign: "center",
+};
+
 header.defaultProps = {
     title:"Task Tracker",
 };
